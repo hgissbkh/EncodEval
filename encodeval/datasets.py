becc53ddcef97a32c0f9e8968db60d21bea7ec76
gisserotboukhlef <gisserotboukhlef@login7.head.adastra.cines.fr>
2025-07-03 16:51:33 +0200

    update

--- encodeval/datasets.py
+++ encodeval/datasets.py
@@ -29,32 +29,13 @@ def load_dataset(*args, **kwargs) -> DatasetDict:
 # Sequence classification
 #========================
 
-def mnli_m() -> DatasetDict:
-    dataset = load_dataset("nyu-mll/glue", "mnli")
-    dataset["test"] = dataset["validation_matched"]
+def sst2() -> DatasetDict:
+    dataset = load_dataset("nyu-mll/glue", "sst2")
+    dataset["test"] = dataset["validation"]
     dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
     dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
-    del dataset["validation_matched"], dataset["validation_mismatched"], dataset["test_matched"], dataset["test_mismatched"]
-    dataset = dataset.map(
-        lambda example: {
-            "text": f"Premise: {example['premise']}\nHypothesis: {example['hypothesis']}",
-        },
-        remove_columns=["premise", "hypothesis", "idx"]
-    )
-    return dataset
-
-def mnli_mm() -> DatasetDict:
-    dataset = load_dataset("nyu-mll/glue", "mnli")
-    dataset["test"] = dataset["validation_mismatched"]
-    dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
-    dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
-    del dataset["validation_matched"], dataset["validation_mismatched"], dataset["test_matched"], dataset["test_mismatched"]
-    dataset = dataset.map(
-        lambda example: {
-            "text": f"Premise: {example['premise']}\nHypothesis: {example['hypothesis']}",
-        },
-        remove_columns=["premise", "hypothesis", "idx"]
-    )
+    dataset = dataset.rename_column("sentence", "text")
+    dataset = dataset.remove_columns(["idx"])
     return dataset
 
 def qqp() -> DatasetDict:
@@ -70,25 +51,17 @@ def qqp() -> DatasetDict:
     )
     return dataset
 
-def sst2() -> DatasetDict:
-    dataset = load_dataset("nyu-mll/glue", "sst2")
-    dataset["test"] = dataset["validation"]
+def mnli_m() -> DatasetDict:
+    dataset = load_dataset("nyu-mll/glue", "mnli")
+    dataset["test"] = dataset["validation_matched"]
     dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
     dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
-    dataset = dataset.rename_column("sentence", "text")
-    dataset = dataset.remove_columns(["idx"])
-    return dataset
-
-def stsb() -> DatasetDict:
-    dataset = load_dataset("nyu-mll/glue", "stsb")
-    dataset["test"] = dataset["validation"]
-    dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
-    dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
+    del dataset["validation_matched"], dataset["validation_mismatched"], dataset["test_matched"], dataset["test_mismatched"]
     dataset = dataset.map(
         lambda example: {
-            "text": f"Sentence 1: {example['sentence1']}\nSentence 2: {example['sentence2']}",
+            "text": f"Premise: {example['premise']}\nHypothesis: {example['hypothesis']}",
         },
-        remove_columns=["sentence1", "sentence2", "idx"]
+        remove_columns=["premise", "hypothesis", "idx"]
     )
     return dataset
 
@@ -97,14 +70,14 @@ def stsb() -> DatasetDict:
 # Token classification
 #=====================
 
+def ontonotes() -> DatasetDict:
+    dataset = load_dataset("hgissbkh/ontonotes5", trust_remote_code=True)
+    return dataset
+
 def conll2003_en() -> DatasetDict:
     dataset = load_dataset("hgissbkh/conll2003-en")
     dataset = dataset.rename_column("words", "tokens")
     dataset = dataset.rename_column("ner", "tags")
-    return dataset
-
-def ontonotes() -> DatasetDict:
-    dataset = load_dataset("hgissbkh/ontonotes5", trust_remote_code=True)
     return dataset
 
 def uner_en() -> DatasetDict:
@@ -129,13 +102,6 @@ def squad() -> DatasetDict:
 def squad_v2() -> DatasetDict:
     dataset = load_dataset("rajpurkar/squad_v2")
     dataset = dataset.remove_columns(["id", "title"])
-    dataset["test"] = dataset["validation"]
-    dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
-    dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
-    return dataset
-
-def qed() -> DatasetDict:
-    dataset = load_dataset("hgissbkh/qed")
     dataset["test"] = dataset["validation"]
     dataset_train_val = dataset["train"].train_test_split(train_size=0.95, seed=42)
     dataset["train"], dataset["validation"] = dataset_train_val["train"], dataset_train_val["test"]
